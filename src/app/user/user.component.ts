18d8ed38bc87382a6e1a730928c0387dc74474c8
Steven Kauyedauty <skauyedauty@gmail.com>
2017-03-02 14:43:46 -0700

    removed syntax issue on form need to improve/reduce form code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,24 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserAuthService } from './user-auth.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+
   signUpForm: FormGroup;
   private first_name: FormControl;
   private last_name: FormControl;
   private email: FormControl;
   private password: FormControl;
-  constructor(private authService: UserAuthService) { }
+
+  constructor(private authService: UserAuthService, private router: Router) { }
 
   ngOnInit() {
-    this.first_name = new FormControl(Validators.required);
-    this.last_name = new FormControl(Validators.required);
-    this.email = new FormControl(Validators.required);
-    this.password = new FormControl(Validators.required);
+    this.first_name = new FormControl(
+      this.authService.newUser.first_name,
+      Validators.required
+    );
+    this.last_name = new FormControl(
+      this.authService.newUser.last_name,
+      Validators.required
+    );
+    this.email = new FormControl(
+      this.authService.newUser.email,
+      Validators.required
+    );
+    this.password = new FormControl(
+      this.authService.newUser.password,
+      Validators.required
+    );
     this.signUpForm = new FormGroup({
       first_name: this.first_name,
       last_name: this.last_name,
@@ -28,7 +44,14 @@ export class UserComponent implements OnInit {
   }
 
   registerUser(formValues) {
-     this.authService.signUpUser(formValues.first_name, formValues.last_name, formValues.email, formValues.password);
+    if (this.signUpForm.valid) {
+        this.authService.signUpUser(
+          formValues.first_name,
+          formValues.last_name,
+          formValues.email,
+          formValues.password);
+        this.router.navigate(['dashboard']);
+    }
    console.log('clicked');
   }
 
