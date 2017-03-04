86591240099eeeb57eb792ebde317ce03a87a316
Steven Kauyedauty <skauyedauty@gmail.com>
2017-03-04 13:59:38 -0700

    setting up node and express

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 const express = require('express');
-const router = express.Router();
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-// declare axios for making http requests
-const axios = require('axios');
+// Get our API routes
+const api = require('./server/routes/api');
 
-/* GET api listing. */
-router.get('/', (req, res) => {
-  res.send('api works');
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Get all posts
-router.get('/posts', (req, res) => {
-  // Get posts from the mock api
-  // This should ideally be replaced with a service that connects to MongoDB
-  axios.get(`${API}/posts`)
-    .then(posts => {
-      res.status(200).json(posts.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
-});
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
 
-module.exports = router;
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
