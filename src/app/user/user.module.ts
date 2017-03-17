64c25520bb84b9c211315c5e5a6f7b9778643d23
Steven Kauyedauty <skauyedauty@gmail.com>
2017-03-16 23:54:29 -0600

    unchecked files retrying lazy loading

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { userRoutes } from './user.routes';
+import { ProfileComponent } from '../profile/profile.component';
+
 @NgModule({
     imports: [
-
+        CommonModule,
+        RouterModule.forChild(userRoutes)
     ],
     declarations: [
-
+        ProfileComponent
     ],
     providers: [
 
---
+++ src/app/user/user.routes.ts
@@ -0,0 +1,7 @@
+import { ProfileComponent } from '../profile/profile.component';
+import { UserComponent } from './user.component';
+
+export const userRoutes = [
+    {path: 'profile', component: ProfileComponent} //user/profile
+];
+
