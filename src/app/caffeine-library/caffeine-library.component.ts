b38da6f3ea13045b1f2b24f5d455276a927a67eb
Steven Kauyedauty <skauyedauty@gmail.com>
2017-03-21 22:34:43 -0600

    refactoring caffeine library

--- src/app/caffeine-library/caffeine-library.component.ts
+++ src/app/caffeine-library/caffeine-library.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core'; 
+import { Component, OnInit, OnChanges, Input } from '@angular/core'; 
 
 import { CaffeineModel } from './caffeine-model';
 import { CaffeineListService } from './caffeine-list.service';
@@ -8,9 +8,11 @@ import { CaffeineListService } from './caffeine-list.service';
   templateUrl: './caffeine-library.component.html',
   styleUrls: ['./caffeine-library.component.css']
 })
-export class CaffeineLibraryComponent implements OnInit {
+export class CaffeineLibraryComponent implements OnInit, OnChanges {
   errorMessage: string;
   caffDrinks: CaffeineModel[]; // assign to this array
+  filterBy: string = 'all';
+  visibleCaffDrinks: CaffeineModel[]; 
 
   constructor(private _caffeineListService: CaffeineListService) { }
 
@@ -21,6 +23,23 @@ export class CaffeineLibraryComponent implements OnInit {
         caffDrinks => this.caffDrinks = caffDrinks,
         error => this.errorMessage = <any> error);
 
+  }
+
+  ngOnChanges() {
+    if(this.caffDrinks) {
+      this.filterCaffeineDrinks(this.filterBy);
+    }
+  }
+
+  filterCaffeineDrinks(filter) {
+    if(filter === 'all') {
+      this.visibleCaffDrinks = this.caffDrinks.slice(0);
+    } else {
+      this.visibleCaffDrinks = this.caffDrinks.filter(drink => {
+        console.log('drink', drink)
+        return drink.name.toLocaleLowerCase() === filter;
+      });
+    }
   }
 
   selectCaffeineDrink(drink) {
---
+++ src/app/shared/common/caffeineDrink.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'drinkType'})
+
+export class DrinkPipe implements PipeTransform {
+    transform(value: number):string {
+        switch(value) {
+            case 1: return 'all'
+            case 2: return 'coffee'
+            case 3: return 'shot'
+            case 4: return 'tea'
+            case 5: return 'energy'
+            default: return value.toString();
+        }
+    }
+}
